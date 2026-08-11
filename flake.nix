191848d00eee54653404f6f96d444ad86d0df047
Mykey <mykey.discord@gmail.com>
2026-08-11 10:08:47 +0800

    chore: simplify Nix development shell

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Development shell for Bear Sec Bot";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -6,25 +8,22 @@
 
   outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShells.default = mkShell {
-          buildInputs = [
-            (python312.withPackages (p:
-              with p; [
-                discordpy
-                python-dotenv
-                pytest
-                aiohttp
-                aiofiles
-                cryptography
-              ]))
-            black
-            lightning-loop
-            ruff
-            basedpyright
-            pyright
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312.withPackages (pythonPackages:
+          with pythonPackages; [
+            discordpy
+            python-dotenv
+          ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.ruff
+            pkgs.basedpyright
           ];
         };
-        formatter = nixfmt-tree;
+
+        formatter = pkgs.nixfmt-tree;
       });
 }
